0e8e795a0eaa1e7debdcc8d9ab4cf2ca5dcbd722
Crip-ri <artmeln19@gmail.com>
2025-01-23 12:02:09 +0200

    Add binnary_search

--- main.cpp
+++ main.cpp
@@ -12,6 +12,43 @@ int rand_range()
 	return rand();
 }
 
+int search(int* arr, int lenght, int value)
+{
+	for(int i = 0; i < lenght; i++)
+	{
+		if(value == arr[i])
+			return i;
+	}
+
+	return -1;
+}
+
+int binnary_search(int* arr, int lenght, int value)
+{
+	int right = lenght, left = 0;
+
+	while(right > left)
+	{
+		std::cout << "Left: " << left << " Right: " << right << std::endl;
+		int middle = (right + left) / 2;
+
+		if(arr[middle] > value)
+		{
+			right = middle - 1;
+		}
+		else if(arr[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else if(arr[middle] == value)
+		{
+			return middle;
+		}
+	}
+
+	return -1;
+}
+
 void bubble_sort(int* arr, int lenght)
 {
 	for(int j = 0; j < lenght; j++)
@@ -33,6 +70,8 @@ void bubble_sort(int* arr, int lenght)
                         break;
         }
 }
+
+
 
 int main()
 {
@@ -58,6 +97,11 @@ int main()
                 std::cout << arr[i] << "\t";
 
 	std::cout << std::endl;
+
+	int value = 0;
+	std::cin >> value;
+	
+	std::cout << binnary_search(arr, n, value) << std::endl;
 
 	std::cout << rand() << std::endl;
 	delete[] arr;
