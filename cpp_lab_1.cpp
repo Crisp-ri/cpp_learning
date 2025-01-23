2a1de401d33dfc0a4ce145973b91f821c2961616
Crip-ri <artmeln19@gmail.com>
2025-01-23 19:10:04 +0200

    Add the 5 tasks of the first labdc

---
+++ cpp_lab_1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+
+void print_hello_world()
+{
+	std::cout << "Hello, World!\n";
+}
+
+void getting_a_name()
+{
+	std::cout << "Enter a name: ";
+	std::string name;
+	std::getline(std::cin, name);
+	std::cout << "Hello, " << name << "!\n";
+}
+
+void calculating()
+{
+	int a = 0, b = 0;
+	std::cout << "Write first number: ";
+	std::cin >> a;
+	std::cout << "Write second number: ";
+	std::cin >> b;
+
+	std::cout << "The sum: " << a + b << std::endl;
+	std::cout << "The differnce: " << a - b << std::endl;
+	std::cout << "The product: " << a * b << std::endl;
+}
+
+void volume_of_the_parallelepiped()
+{
+	int a = 0, b = 0, c = 0;
+	std::cout << "Enter size of first side: ";
+	std::cin >> a;
+	std::cout << "Enter size of second side: ";
+	std::cin >> b;
+	std::cout << "Enter size of third side: ";
+        std::cin >> c;
+
+	std::cout << "The volume is " << a * b* c << std::endl;
+}
+
+void area_of_a_parallelepiped()
+{
+	int a = 0, b = 0, c = 0;
+        std::cout << "Enter size of first side: ";
+        std::cin >> a;
+        std::cout << "Enter size of second side: ";
+        std::cin >> b;
+        std::cout << "Enter size of third side: ";
+        std::cin >> c;
+
+        std::cout << "The area is " << 2 * (a * b + b * c + c * a) << std::endl;
+
+}
+
+void info_about_circle()
+{
+	const float pi = 3.14;
+
+	int radius = 0;
+	std::cout << "Enter a radius of the circle: ";
+	std::cin >> radius;
+
+	std::cout << "Area of the circle is " << pi * radius * radius << std::endl;
+	std::cout << "The circle length is " << 2 * pi * radius << std::endl;
+}
+
+int main()
+{
+	print_hello_world();
+
+	getting_a_name();
+	
+	calculating();
+
+	volume_of_the_parallelepiped();
+
+	area_of_a_parallelepiped();
+
+	info_about_circle();
+
+	return 0;
+}
